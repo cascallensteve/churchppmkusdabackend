8614b8fa0cac8398f324479ed5a0f220143e7041
CascallenSteve <cascallensteve@gmail.com>
2026-08-20 17:39:08 +0300

    go slowly

--- mkd/wsgi.py
+++ mkd/wsgi.py
@@ -1,10 +1,5 @@
-"""
-Django WSGI config for mkd project.
-"""
-
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mkd.settings')
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mkd.settings")
 application = get_wsgi_application()
