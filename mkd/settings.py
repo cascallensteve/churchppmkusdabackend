c718a9fb5b57b4ee608ba0b1147acf5dccc57bec
CascallenSteve <cascallensteve@gmail.com>
2026-08-20 17:42:52 +0300

    see again

--- mkd/settings.py
+++ mkd/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'mkd.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'mkd' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- mkd/urls.py
+++ mkd/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='running.html'), name='home'),
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
