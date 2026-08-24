578f54ada67542f974e7c63e571b051aeb54b1d7
CascallenSteve <cascallensteve@gmail.com>
2026-08-24 11:23:12 +0300

    pushed with admin dahbaord

--- mkd/settings.py
+++ mkd/settings.py
@@ -114,7 +114,8 @@ CORS_ALLOWED_ORIGINS = [
     'https://mkusda-church-treasury-fronted.vercel.app',
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-     'http://127.0.0.1:5174',
+    'http://localhost:5174',
+    'http://127.0.0.1:5174',
 ]
 CORS_ALLOW_CREDENTIALS = True
 
